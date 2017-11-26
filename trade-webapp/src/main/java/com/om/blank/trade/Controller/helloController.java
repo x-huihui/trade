add1f6f4ecdc5f83fe36f52a4085e7151f22e7ac
x-huihui <272045951@qq.com>
2017-11-26 23:55:25 +0800

    slf4j + log4j

---
+++ trade-webapp/src/main/java/com/om/blank/trade/Controller/helloController.java
@@ -0,0 +1,18 @@
+package com.om.blank.trade.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by KeKe on 2017/11/26.
+ */
+public class helloController {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(helloController.class);
+        logger.info("hello world");
+    }
+    public static void hello() {
+        Logger logger = LoggerFactory.getLogger(helloController.class);
+        logger.info("hello world");
+    }
+}
---
+++ trade-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page import="com.om.blank.trade.Controller.helloController" %><%--
+  Created by IntelliJ IDEA.
+  User: KeKe
+  Date: 2017/11/26
+  Time: 下午 11:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+hello<% helloController.hello();%>
+</body>
+</html>
