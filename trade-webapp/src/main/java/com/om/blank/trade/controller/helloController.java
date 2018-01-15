7ec8f23425ab797cab80efa3ba249b98154a38b9
x-huihui <272045951@qq.com>
2018-01-15 22:20:28 +0800

    改用一个模块

--- trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
+++ trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
@@ -1,13 +1,9 @@
 package com.om.blank.trade.controller;
 
-import com.om.blank.trade.db.dao.userMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -23,30 +19,13 @@ import java.io.InputStream;
  * Created by KeKe on 2017/11/26.
  */
 @Controller
-@RequestMapping("/trade/hello")
+@RequestMapping("/hello/")
 public class helloController {
     @GetMapping("/c")
     @ResponseBody
-    public String hello(Model model) {
+    public String helloc(Model model) {
 
         Logger logger = LoggerFactory.getLogger(helloController.class);
-        try {
-            String resource = "mybatis.xml";
-            InputStream inputStream = null;
-            inputStream = Resources.getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory =
-                    new SqlSessionFactoryBuilder().build(inputStream);
-            SqlSession sqlSession = sqlSessionFactory.openSession();
-            userMapper mapper = sqlSession.getMapper(userMapper.class);
-            logger.info(mapper.selectByPrimaryKey(1L).toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-       /* UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("xiaoming", "xiaoming");
-        usernamePasswordToken.getPrincipal();
-        usernamePasswordToken.getCredentials();
-        Subject subject = SecurityUtils.getSubject();
-        subject.login(usernamePasswordToken);*/
         return "hello c";
     }
 }
