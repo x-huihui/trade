57219a7b590ea4e55ea87ae3da9a68974cfa7e58
x-huihui <272045951@qq.com>
2018-01-17 23:10:37 +0800

    mybatis-spring

--- trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
+++ trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
@@ -1,11 +1,13 @@
 package com.om.blank.trade.controller;
 
+import com.om.blank.trade.mapper.userMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +23,16 @@ import java.io.InputStream;
 @Controller
 @RequestMapping("/hello/")
 public class helloController {
+
+    @Autowired
+    private userMapper u;
+
     @GetMapping("/c")
     @ResponseBody
     public String helloc(Model model) {
 
         Logger logger = LoggerFactory.getLogger(helloController.class);
+        logger.info(u.selectByPrimaryKey(1L).toString());
         return "hello c";
     }
 }
