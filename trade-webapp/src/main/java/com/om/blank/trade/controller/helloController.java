b7b1f92a1c4937543b08e599b6caec5f7db7451c
x-huihui <272045951@qq.com>
2018-01-21 18:57:08 +0800

    spring shiro

--- trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
+++ trade-webapp/src/main/java/com/om/blank/trade/controller/helloController.java
@@ -1,38 +1,42 @@
 package com.om.blank.trade.controller;
 
-import com.om.blank.trade.mapper.userMapper;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by KeKe on 2017/11/26.
  */
+
+import com.om.blank.trade.mybatis.mapper.UserMapper;
+import com.om.blank.trade.mybatis.model.UserExample;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 @RequestMapping("/hello/")
 public class helloController {
 
     @Autowired
-    private userMapper u;
+    private UserMapper u;
+
+    private static Logger LOGGER = LoggerFactory.getLogger(helloController.class);
 
     @GetMapping("/c")
     @ResponseBody
-    public String helloc(Model model) {
-
-        Logger logger = LoggerFactory.getLogger(helloController.class);
-        logger.info(u.selectByPrimaryKey(1L).toString());
+    public String helloc(@RequestParam("account") String account, @RequestParam("password") String password) {
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken();
+        usernamePasswordToken.setUsername(account);
+        usernamePasswordToken.setPassword(password.toCharArray());
+        subject.login(usernamePasswordToken);
+        LOGGER.info("hello c");
         return "hello c";
     }
 }
--- trade-webapp/src/main/java/com/om/blank/trade/util/MysqlRealm.java
+++ trade-webapp/src/main/java/com/om/blank/trade/util/MysqlRealm.java
@@ -1,13 +1,31 @@
 package com.om.blank.trade.util;
 
+import com.om.blank.trade.mybatis.mapper.UserMapper;
+import com.om.blank.trade.mybatis.model.User;
+import com.om.blank.trade.mybatis.model.UserExample;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class MysqlRealm extends AuthenticatingRealm {
 
+    @Autowired
+    UserMapper userMapper;
+
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
-        SimpleAuthenticationInfo simpleAccount = new SimpleAuthenticationInfo(usernamePasswordToken.getUsername(), usernamePasswordToken.getUsername(), "用户名密码相同");
+        UserExample example = new UserExample();
+        example.or().andAccountEqualTo(usernamePasswordToken.getUsername());
+        List<User> users = userMapper.selectByExample(example);
+        User user;
+        if (users != null && users.size() > 0){
+            user = users.get(0);
+        } else {
+            throw new AuthenticationException();
+        }
+        SimpleAuthenticationInfo simpleAccount = new SimpleAuthenticationInfo(user.getAccount(), user.getPassword(), "MysqlRealm");
         return simpleAccount;
     }
 }
