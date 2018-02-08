d3c4fc66abfaa4ebdca76d27f9e5dbc5d5ccdb73
x-huihui <272045951@qq.com>
2018-02-08 22:09:44 +0800

    优化

---
+++ tools/src/main/java/com/om/blank/tools/mybatis/plugins/ExamplePlugins.java
@@ -0,0 +1,19 @@
+package com.om.blank.tools.mybatis.plugins;
+
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+public class ExamplePlugins extends PluginAdapter {
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+}
